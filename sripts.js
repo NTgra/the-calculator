0d6fe56b2c012327aeadf3a56fe8953f46b414a0
NTgra <nektotigra@gmail.com>
2023-03-02 13:08:43 -0700

    Add function "operate"

--- sripts.js
+++ sripts.js
@@ -15,7 +15,27 @@ function divide (numberOne, numberTwo) {
     
     return numberOne/numberTwo;
 }
+
+function operate(operation, numberOne, numberTwo){
+    if (operation==="add"){
+        return add(numberOne,numberTwo);
+    }
+    if (operation==="subtract"){
+        return subtract(numberOne,numberTwo);
+    }
+    if (operation==="multiply"){
+        return multiply(numberOne,numberTwo);
+    }
+    if (operation==="divide"){
+        return divide(numberOne,numberTwo);
+    }
+}
+
 console.log(add(1,2))
 console.log(subtract(1,2))
 console.log(multiply(13,2))
 console.log(divide(13,29))
+console.log(operate("add",1,2));
+console.log(operate("subtract",1,2));
+console.log(operate("multiply",13,2));
+console.log(operate("divide",13,29));
