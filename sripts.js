0b3abf96cb21009b0e8c5744b4474064de086948
NTgra <nektotigra@gmail.com>
2023-03-02 13:01:30 -0700

    Add the "add", "subtract", "multiply" and "divide"

---
+++ sripts.js
@@ -0,0 +1,21 @@
+function add(numberOne, numberTwo) {
+    
+    return numberOne+numberTwo;
+}
+function subtract (numberOne, numberTwo) {
+    
+    return numberOne-numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+    
+    return numberOne*numberTwo;
+}
+
+function divide (numberOne, numberTwo) {
+    
+    return numberOne/numberTwo;
+}
+console.log(add(1,2))
+console.log(subtract(1,2))
+console.log(multiply(13,2))
+console.log(divide(13,29))
