fa86cdf63b7ea42005d4337bf7c472820068bb0c
NTgra <nektotigra@gmail.com>
2023-03-03 13:49:13 -0700

    Add event listeners to the numeric keypad

--- sripts.js
+++ sripts.js
@@ -31,11 +31,43 @@ function operate(operation, numberOne, numberTwo){
     }
 }
 
-console.log(add(1,2))
-console.log(subtract(1,2))
-console.log(multiply(13,2))
-console.log(divide(13,29))
-console.log(operate("add",1,2));
-console.log(operate("subtract",1,2));
-console.log(operate("multiply",13,2));
-console.log(operate("divide",13,29));
+//
+const element = document.createElement("div");
+function appendNumber(theNumber) {   
+    
+
+    //console.log(theNumber)
+        // Create a text node:
+    const textnode = document.createTextNode(theNumber);
+    
+    //console.log(workingNumber);
+    node.appendChild(textnode);
+        
+    document.getElementById("lcd-screen").appendChild(element);
+}
+// populating the numbers keypad with event listeners:
+  let el = "";
+  let keyPad = "num";
+for (let i=0; i<10; i++){
+  
+  let keyPadNumber = el+i;
+  let keyPadValue = keyPad+i;
+    // console.log(keyPadNumber + " " + keyPadValue)
+    keyPadNumber = document.getElementById(keyPadValue);
+    keyPadNumber.addEventListener(
+    "click",
+    function () {
+      appendNumber(i);
+    }
+  );
+}
+
+
+// console.log(add(1,2))
+// console.log(subtract(1,2))
+// console.log(multiply(13,2))
+// console.log(divide(13,29))
+// console.log(operate("add",1,2));
+// console.log(operate("subtract",1,2));
+// console.log(operate("multiply",13,2));
+// console.log(operate("divide",13,29));
